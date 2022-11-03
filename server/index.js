89beaaffc9fc8852c5911afb3e6916f27e7d48de
Yu Patricio <yupatricio0@gmail.com>
2022-11-03 20:31:11 -0300

    conecto la BD

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const mongoose = require('mongoose');
+const cors     = require('cors');
+const morgan   = require('morgan');
+
+const port = 4000;
+const db = process.env.MONGODB_URI || 'mongodb://127.0.0.1/mmmchanguches';
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => {
+    console.log(`Database connected @${db}`);
+  })
+  .catch(err => {
+    console.error(`Connection error ${err}`);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
