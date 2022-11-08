a0fe74b795b1e78f79bc0f251d4e97e1f4866e3d
Yu Patricio <yupatricio0@gmail.com>
2022-11-08 19:49:22 -0300

    modelo User actualizado

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true, minlenght: 3 },
-  surname: { type: String, required: true},
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now },
-});
+  name: { type: String, lowercase: true, minlenght: 3, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'] },
+  email: { type: String, lowercase: true , unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/, 'is invalid'] },
+  password: { type: String, required: [true, "can't be blank"] },
+}, {timestamps: true});
 
 const User = mongoose.model('user', UserSchema);
 
